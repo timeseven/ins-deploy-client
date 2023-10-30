a6aff520efb6e42b17ed90b3176909aead677794
Even Qian <evenwin88@gmail.com>
2023-10-30 22:16:36 +1100

    feat: deploy url

--- src/utils/baseUrl.ts
+++ src/utils/baseUrl.ts
@@ -1,2 +1,4 @@
-export const BASE_URL = "http://localhost:4000/api";
-export const BASE_URL_SOCKET = "http://localhost:4000/";
+// export const BASE_URL = "http://localhost:4000/api";
+// export const BASE_URL_SOCKET = "http://localhost:4000/";
+export const BASE_URL = "http://ec2-54-66-16-198.ap-southeast-2.compute.amazonaws.com/api";
+export const BASE_URL_SOCKET = "http://ec2-54-66-16-198.ap-southeast-2.compute.amazonaws.com/";
